40fb4ae6b1afb3bf48c2c2c79728f1837e488478
Chitaunhike <t.chitaunhike@accenture.com>
2025-10-27 11:53:23 +0200

    first push

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace FrameworkBuilding;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test] 
+    public void Test2()
+    {
+        Assert.Fail();
+    }
+}
